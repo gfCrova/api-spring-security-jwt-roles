5b0f130f77270101a46b7d38f39d2c4f866fd2eb
gfCrova <giancrova.cl5@gmail.com>
2026-03-21 23:34:19 -0300

    Replace MockBean with MockitoBean in controller test

--- src/test/java/com/example/demogc/presentation/controller/UserControllerSecurityTest.java
+++ src/test/java/com/example/demogc/presentation/controller/UserControllerSecurityTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Set;
@@ -29,7 +29,7 @@ class UserControllerSecurityTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private UserService userService;
 
     @Test
